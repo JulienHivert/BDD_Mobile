4c36c06a3a974b009c1bebe2e6d4c372bb8b1bf7
Julien Hivert <julienhivert.tt@gmail.com>
2018-04-06 08:57:47 +0200

    [ADD] First commit

---
+++ iOS_BDD_Do_It/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  iOS_BDD_Do_It
+//
+//  Created by iem on 30/03/2018.
+//  Copyright © 2018 iem. All rights reserved.
+//
+import Foundation
+
+class Item : Codable{
+    var text: String
+    var checked = false
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+}
